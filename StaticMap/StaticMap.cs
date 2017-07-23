b46dd057a18774b0f6111c05f1ab25bba8638279
Victor D. Montero <victordanielmontero@gmail.com>
2017-07-22 22:35:27 -0400

    Agregando archivos fuentes

--- StaticMap/StaticMap.cs
+++ StaticMap/StaticMap.cs
@@ -1,13 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace StaticMap
 {
-    public class StaticMap
+    /*!
+     * \brief Helper class to download an static map image
+     * 
+     * This class provides some methods to downloads static map image from google.
+     * You need a Google Api Key, in order to use the service.
+     */
+    public static class StaticMap
     {
 
+        public static string GoogleKey { get; set; }/*! https://developers.google.com/maps/documentation/javascript/get-api-key */
+        public static string ApiUrl { get; set; }
+
+        /*!
+         * \brief Downloads a png image with a resolution up to 640 x 480
+         * 
+         * This function returns an image serialized as a byte array.
+         * \param[in] latitude.
+         * \param[in] longitude.
+         * \param[in] key: this is a free developer google key. Its mandatory to have it, in order to use
+         * the google static maps service.
+         * \param[in] options: a key value pair collection, containing all optional parameters.
+         * 
+         * For more information about static maps, see: https://developers.google.com/maps/documentation/static-maps/intro
+         * 
+         */
+        public static byte[] DownloadMap(double latitude, double longitude, Dictionary<string, string> options = null)
+        {
+            string url = string.Empty;
+            WebClient client = new WebClient();
+
+            if (options == null)
+                options = new Dictionary<string, string>();
+
+            if (!options.ContainsKey("center"))
+                options.Add("center", string.Format("{0},{1}", latitude, longitude));
+
+            if (!options.ContainsKey("size"))
+                options.Add("size", string.Format("{0}x{1}", 1024, 768));
+
+            if (!options.ContainsKey("zoom"))
+                options.Add("zoom", string.Format("{0}", 19));
+
+            if (!options.ContainsKey("markers"))
+                options.Add("markers", string.Format("color:red|{0},{1}", latitude, longitude));
+
+            url = ApiUrl;
+            url += "?";
+
+            foreach (KeyValuePair<string, string> pair in options)
+            {
+                url += string.Format("{0}={1}", pair.Key, pair.Value);
+                url += "&";
+            }
+
+            url += string.Format("key={0}", GoogleKey);
+
+            return client.DownloadData(url);
+        }
     }
 }
---
+++ StaticMapTest/StaticMapTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StaticMap;
+using System.IO;
+using System.Collections.Generic;
+
+namespace StaticMapTest
+{
+    [TestClass]
+    public class StaticMapTest
+    {
+        [TestMethod]
+        public void DownloadMapTest()
+        {
+            StaticMap.StaticMap.ApiUrl = "https://maps.googleapis.com/maps/api/staticmap";
+            StaticMap.StaticMap.GoogleKey = "AIzaSyDlHLSiBVLFijxMxz846ZAraExGOjYFLzs";
+
+            var result = StaticMap.StaticMap.DownloadMap(18.452343, -69.952257);
+            Assert.AreNotEqual(null, result);
+            File.WriteAllBytes("image.png", result);
+        }
+
+        [TestMethod]
+        public void DownloadMapTestWithExtra()
+        {
+            StaticMap.StaticMap.ApiUrl = "https://maps.googleapis.com/maps/api/staticmap";
+            StaticMap.StaticMap.GoogleKey = "AIzaSyDlHLSiBVLFijxMxz846ZAraExGOjYFLzs";
+
+            Dictionary<string, string> ops = new Dictionary<string, string>();
+
+            ops.Add("maptype", "satellite");
+
+            var result = StaticMap.StaticMap.DownloadMap(18.452343, -69.952257, ops);
+            Assert.AreNotEqual(null, result);
+            File.WriteAllBytes("imageWithParam.png", result);
+        }
+    }
+}
